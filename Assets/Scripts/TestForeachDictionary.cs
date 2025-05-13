2449a9dabfd94f4fef6a11f9d24ffb5e2d01c6e5
yijianyouxie <haibinxiaochenga@163.com>
2025-05-13 16:19:37 +0800

    合并角色mesh和武器mesh

---
+++ Assets/Scripts/TestForeachDictionary.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestForeachDictionary : MonoBehaviour {
+
+    private Dictionary<int, int> dic = new Dictionary<int, int>(4);
+	// Use this for initialization
+	void Start () {
+        dic[1] = 100;
+        dic[2] = 200;
+        dic[3] = 300;
+        dic[4] = 400;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            foreach(var item in dic)
+            {
+                Debug.LogError("====================");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TestSkinBones.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 用于测试将skinnedMeshRenderer的bones数组中
+/// 移除一部分后的表现是什么样子的
+/// </summary>
+public class TestSkinBones : MonoBehaviour {
+
+    public SkinnedMeshRenderer smr;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(0,0,100,50), "RemoveBones"))
+        {
+            Transform[] newBones = new Transform[0];
+            smr.bones = newBones;
+        }
+    }
+}
